db63bda1304257bc3a0ef25723da17439e2d8643
Esther <3444104242@qq.com>
2025-12-16 14:21:55 +0800

    feat:cp-nav-bar功能标题和右侧文字和点击自定义事件

--- src/components/CpNavBar.vue
+++ src/components/CpNavBar.vue
@@ -1,6 +1,28 @@
+<script lang="ts" setup>
+// 通过defineProps 获取父组件传递的参数 实现标题和右侧文字的设置
+defineProps<{
+  title?: string
+  rightText?: string
+}>()
+
+const emit = defineEmits<{
+  (event: 'click-right'): void
+}>()
+
+const onClickRight = () => {
+  emit('click-right')
+}
+</script>
+
 <template>
   <div class="login-page">
-    <van-nav-bar fixed left-arrow title="登录" right-text="注册"></van-nav-bar>
+    <van-nav-bar
+      fixed
+      left-arrow
+      :title="title"
+      :right-text="rightText"
+      @click-right="onClickRight"
+    ></van-nav-bar>
   </div>
 </template>
 
--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,9 +1,17 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const handleRightClick = () => {
+  console.log('点击了注册')
+}
+</script>
 
 <template>
   <h1>登录</h1>
   <van-button type="success">登录</van-button>
-  <cp-nav-bar></cp-nav-bar>
+  <cp-nav-bar
+    title="登录"
+    rightText="注册"
+    @click-right="handleRightClick"
+  ></cp-nav-bar>
 </template>
 
 <style scoped lang="scss"></style>
