89f5936304bcaae96404cb6a9ad674e54b0f16ae
Esther <3444104242@qq.com>
2025-12-16 13:57:48 +0800

    feat:cp-nav-bar结构、样式

---
+++ src/components/CpNavBar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="login-page">
+    <van-nav-bar fixed left-arrow title="登录" right-text="注册"></van-nav-bar>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+:deep() {
+  .van-nav-bar {
+    &__arrow {
+      color: var(--cp-text1);
+      font-size: 18px;
+    }
+    &__title {
+      font-size: 15px;
+    }
+  }
+}
+</style>
--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -3,6 +3,7 @@
 <template>
   <h1>登录</h1>
   <van-button type="success">登录</van-button>
+  <cp-nav-bar></cp-nav-bar>
 </template>
 
 <style scoped lang="scss"></style>
