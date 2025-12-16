e442aadd99acc7cf289d8f7c69ed5a77b2aa460c
Esther <3444104242@qq.com>
2025-12-16 12:49:56 +0800

    401处理

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,5 +1,6 @@
+import router from '@/router/index.js'
 import { useUserStore } from '@/stores/index.js'
-import axios from 'axios'
+import axios, { AxiosError } from 'axios'
 import { showToast } from 'vant'
 
 const instance = axios.create({
@@ -30,8 +31,21 @@ instance.interceptors.response.use(
     // TODO 4. 摘取核心响应数据
     return res.data
   },
-  (err) => {
+  (err: AxiosError) => {
     // TODO 5. 处理401错误
+    if (err.response?.status === 401) {
+      showToast('登录过期，请重新登录')
+      // 清空用户信息
+      const userStore = useUserStore()
+      userStore.removeUser()
+      // 跳转到登录页
+      router.push({
+        path: '/login',
+        query: {
+          returnUrl: router.currentRoute.value.fullPath
+        }
+      })
+    }
     return Promise.reject(err)
   }
 )
