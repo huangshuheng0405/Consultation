3c0d4ef27a8fdafd981b00d3802505f995ff36c5
Esther <3444104242@qq.com>
2025-12-16 17:51:09 +0800

    feat:svg组件封装、密码显示

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -76,6 +76,9 @@ const onSendCode = async () => {
 onUnmounted(() => {
   clearInterval(timer)
 })
+
+// 控制密码可见与不可见
+const isShow = ref(false)
 </script>
 
 <template>
@@ -105,8 +108,16 @@ onUnmounted(() => {
         v-model="password"
         :rules="passwordRules"
         placeholder="请输入密码"
-        type="password"
-      ></van-field>
+        :type="isShow ? 'text' : 'password'"
+      >
+        <template #button>
+          <cp-icon
+            :name="`login-eye-${isShow ? 'on' : 'off'}`"
+            @click="isShow = !isShow"
+            style="margin-right: 10px"
+          ></cp-icon>
+        </template>
+      </van-field>
       <van-field
         v-else
         placeholder="短信验证码"
@@ -139,9 +150,7 @@ onUnmounted(() => {
         <a href="javascript:;">忘记密码？</a>
       </div>
     </van-form>
-    <svg aria-hidden="true">
-      <use href="#icon-consult-alipay" />
-    </svg>
+    <cp-icon name="consul t-wechat"></cp-icon>
     <!-- 底部 -->
     <div class="login-other">
       <van-divider>第三方登录</van-divider>
