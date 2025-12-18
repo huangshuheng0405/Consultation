ab4c3f32ef832348584eb428138412eab7c1151c
Esther <3444104242@qq.com>
2025-12-18 11:41:27 +0800

    feat:首页模块-知识类型定义

--- src/views/Home/components/KnowledgeList.vue
+++ src/views/Home/components/KnowledgeList.vue
@@ -1,6 +1,11 @@
 <script setup lang="ts">
 import KnowledgeCard from './KnowledgeCard.vue'
 import { ref } from 'vue'
+import { KnowledgeType } from '@/types/consult.js'
+
+defineProps<{
+  type: KnowledgeType
+}>()
 
 // 加载中状态
 const loading = ref(false)
