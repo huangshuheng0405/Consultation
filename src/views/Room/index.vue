d612d31ebd8e642a01d444a830d8049641b00685
Esther <3444104242@qq.com>
2025-12-23 12:03:17 +0800

    feat:socket.io-client使用

--- src/views/Room/index.vue
+++ src/views/Room/index.vue
@@ -2,6 +2,21 @@
 import RoomStatus from '@/views/Room/components/RoomStatus.vue'
 import RoomAction from '@/views/Room/components/RoomAction.vue'
 import RoomMessage from '@/views/Room/components/RoomMessage.vue'
+import io from 'socket.io-client'
+
+// 建立连接
+const socket = io('http://localhost:3000')
+socket.on('connect', () => {
+  console.log('connect success')
+  socket.emit('chat message', 'hello world')
+})
+socket.on('chat message', (msg) => {
+  console.log(msg)
+})
+
+socket.on('disconnect', () => {
+  console.log('disconnect')
+})
 </script>
 
 <template>
